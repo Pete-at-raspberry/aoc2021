40309b22eec0b79d0cc0a6bc11216815ed95f314
Pete at Raspberry <peter.jarvis@raspberrysoftware.com>
2022-01-25 17:15:21 +0000

    Start of Day 23

---
+++ AOC2021/23/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _23
+{
+    class Program
+    {
+        /* Starting positions: Just represent as Strings. 
+         * Example
+         *  #############
+            #...........#
+            ###B#C#B#D###
+              #A#D#C#A#
+              #########
+
+        * My Input
+        * #############
+          #...........#
+          ###B#C#A#B###
+            #C#D#D#A#
+            #########
+        */
+        static String input1 = "BCBD";
+        static String input2 = "ADCA";
+        //static String input1 = "BCAB";
+        //static String input2 = "CDDA";
+
+
+        static void Main(string[] args)
+        {
+            Board bd = SetupBoard();
+
+            // Play...
+        }
+
+        static private Board SetupBoard()
+        {
+            // Initialise from the input strings.
+        }
+    }
+
+    public class Board
+    {
+
+    }
+}
